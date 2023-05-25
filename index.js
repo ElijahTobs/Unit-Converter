3d9eee3b25adff46a1904717a11993f737247c0c
Comfort Ige <eligenius@gmail.com>
2023-05-25 22:25:50 +0100

    Add volume and mass functionality

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ massEl.innerHTML = `<h4>Mass (Kilograms/Pounds)</h4>`
 convertBtn.addEventListener("click", function(){
   lengthEl.innerHTML += `
     <p>
-      ${inputEl.value} metres = ${inputEl.value * 3.28084} feet | ${inputEl.value} feet = ${inputEl.value * 0.3048} metres
+      ${inputEl.value} metres = ${(inputEl.value * 3.28084).toFixed(3)} feet | ${inputEl.value} feet = ${(inputEl.value * 0.3048).toFixed(3)} metres
+    </p>
+  `
+  volumeEl.innerHTML += `
+    <p>
+      ${inputEl.value} litre = ${(inputEl.value * 0.264172).toFixed(3)} gallon | ${inputEl.value} gallon = ${(inputEl.value * 3.78541).toFixed(3)} litre
+    </p>
+  `
+
+  massEl.innerHTML += `
+    <p>
+      ${inputEl.value} kilos = ${(inputEl.value * 2.20462).toFixed(3)} pound | ${inputEl.value} pound = ${(inputEl.value * 0.453592).toFixed(3)} kilos
     </p>
   `
 })
