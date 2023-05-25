31968be65c90290a3b22ff9f022b59f5eb49a65d
Comfort Ige <eligenius@gmail.com>
2023-05-25 22:52:27 +0100

    Fix multiple click issue

--- index.js
+++ index.js
@@ -9,22 +9,28 @@ volumeEl.innerHTML = `<h4>Volume (Litres/Gallons)</h4>`
 massEl.innerHTML = `<h4>Mass (Kilograms/Pounds)</h4>`
 
 convertBtn.addEventListener("click", function(){
-  lengthEl.innerHTML += `
-    <p>
-      ${inputEl.value} metres = ${(inputEl.value * 3.28084).toFixed(3)} feet | ${inputEl.value} feet = ${(inputEl.value * 0.3048).toFixed(3)} metres
-    </p>
-  `
-  volumeEl.innerHTML += `
-    <p>
-      ${inputEl.value} litre = ${(inputEl.value * 0.264172).toFixed(3)} gallon | ${inputEl.value} gallon = ${(inputEl.value * 3.78541).toFixed(3)} litre
-    </p>
-  `
+  let num = inputEl.value
+  if (num){
+    lengthEl.innerHTML = `
+      <h4>Length (Metre/Feet)</h4>
+      <p>
+        ${num} metres = ${(num * 3.28084).toFixed(3)} feet | ${num} feet = ${(num * 0.3048).toFixed(3)} metres
+      </p>
+    `
+    volumeEl.innerHTML = `
+      <h4>Volume (Litres/Gallons)</h4>
+      <p>
+        ${num} litre = ${(num * 0.264172).toFixed(3)} gallon | ${num} gallon = ${(num * 3.78541).toFixed(3)} litre
+      </p>
+    `
+    massEl.innerHTML = `
+      <h4>Mass (Kilograms/Pounds)</h4>
+      <p>
+        ${num} kilos = ${(num * 2.20462).toFixed(3)} pound | ${num} pound = ${(num * 0.453592).toFixed(3)} kilos
+      </p>
+    `
+  }
+  
 
-  massEl.innerHTML += `
-    <p>
-      ${inputEl.value} kilos = ${(inputEl.value * 2.20462).toFixed(3)} pound | ${inputEl.value} pound = ${(inputEl.value * 0.453592).toFixed(3)} kilos
-    </p>
-  `
 })
-
 
